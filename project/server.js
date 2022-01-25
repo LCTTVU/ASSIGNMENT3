9709447fd459b1802949231c75b6783b9c9b3852
LCTTVU <lecungtuantranh@student.vu.nl>
2022-01-25 18:43:02 +0100

    server routes

--- project/server.js
+++ project/server.js
@@ -1,96 +1,65 @@
-// ###############################################################################
-// Web Technology at VU University Amsterdam
-// Assignment 3
-//
-// The assignment description is available on Canvas. 
-// Please read it carefully before you proceed.
-//
-// This is a template for you to quickly get started with Assignment 3.
-// Read through the code and try to understand it.
-//
-// Have you read the zyBook chapter on Node.js?
-// Have you looked at the documentation of sqlite?
-// https://www.sqlitetutorial.net/sqlite-nodejs/
-//
-// Once you are familiar with Node.js and the assignment, start implementing
-// an API according to your design by adding routes.
+const express = require("express");
+const app = express();
 
 
-// ###############################################################################
-//
-// Database setup:
-// First: Our code will open a sqlite database file for you, and create one if it not exists already.
-// We are going to use the variable "db' to communicate to the database:
-// If you want to start with a clean sheet, delete the file 'phones.db'.
-// It will be automatically re-created and filled with one example item.
-
-const sqlite = require('sqlite3').verbose();
+const sqlite = require("sqlite3").verbose();
 let db = my_database('./phones.db');
 
-
-// ###############################################################################
-// The database should be OK by now. Let's setup the Web server so we can start
-// defining routes.
-//
-// First, create an express application `app`:
-
-var express = require("express");
-var app = express();
-
-// We need some middleware to parse JSON data in the body of our HTTP requests:
-var bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 
-// ###############################################################################
-// Routes
-// 
-// TODO: Add your routes here and remove the example routes once you know how
-//       everything works.
-// ###############################################################################
 
-// This example route responds to http://localhost:3000/hello with an example JSON object.
-// Please test if this works on your own device before you make any changes.
-
-app.get("/hello", function(req, res) {
-    response_body = {'Hello': 'World'} ;
-
-    // This example returns valid JSON in the response, but does not yet set the
-    // associated HTTP response header.  This you should do yourself in your
-    // own routes!
-    res.json(response_body) ;
+app.get("/phones", async function(req, res) {
+	const phones = await db.getAllPhones();
+	return res.status(200).json({ phones });
 });
 
-// This route responds to http://localhost:3000/db-example by selecting some data from the
-// database and return it as JSON object.
-// Please test if this works on your own device before you make any changes.
-app.get('/db-example', function(req, res) {
-    // Example SQL statement to select the name of all products from a specific brand
-    db.all(`SELECT * FROM phones WHERE brand=?`, ['Fairphone'], function(err, rows) {
+
+app.post("/phones", async function(req, res) {
+	const results = await db.createPhone(req.body);
+	return res.status(201).json({ id: results[0] })
+})
+
+
+app.put("/phones/:id", async function(req,res) {
+	const id = await db.updatePhone(req.params.id, req.body);
+	return res.status(200).json({ id });
+});
+
+
+app.delete("/phones/:id", async function(req, res) {
+	const id = await db.deletePhone(req.params.id);
+	return res.status(200).json({ id });
+})
+
+
+app.get("/test", function(req, res) {
+    return res.status(200).json({ success: true});
+});
+
+app.listen(3000, function() {
+    console.log("Web server up and running");
+});
+
+
+function getAllPhones() {
+
+}
+
+function createPhone(phone) {
+
+}
+
+function updatePhone(id, phone) {
+
+}
+
+function deletePhone(id) {
 	
-    	// TODO: add code that checks for errors so you know what went wrong if anything went wrong
-    	// TODO: set the appropriate HTTP response headers and HTTP response codes here.
-
-    	// # Return db response as JSON
-    	return res.json(rows)
-    });
-});
-
-app.post('/post-example', function(req, res) {
-	// This is just to check if there is any data posted in the body of the HTTP request:
-	console.log(req.body);
-	return res.json(req.body);
-});
+}
 
 
-// ###############################################################################
-// This should start the server, after the routes have been defined, at port 3000:
-
-app.listen(3000);
-console.log("Your Web server should be up and running, waiting for requests to come in. Try http://localhost:3000/hello");
-
-// ###############################################################################
-// Some helper functions called above
 function my_database(filename) {
 	// Conncect to db by opening filename, create filename if it does not exist:
 	var db = new sqlite.Database(filename, (err) => {
